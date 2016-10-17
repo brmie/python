2ea0c4a86200939cf47c4bb99337febcb76f7d7c
boramkim <brmie@hanmail.net>
2016-10-18 03:02:08 +0900

    twice commit

---
+++ firstproject/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/boramkim/Desktop/boram/development/python/practice/django-practice/firstproject/myvenv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
