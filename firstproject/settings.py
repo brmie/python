f408a6cbc90fea818022eb94c88ec5148ccb9b64
boramkim <brmie@hanmail.net>
2016-10-19 01:32:08 +0900

    edit script

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 #     }
 # }
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
