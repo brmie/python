ae5744f0fc8f85318562f787c03b56f49f7b264a
boramkim <brmie@hanmail.net>
2016-10-18 17:30:06 +0900

    add anonymous user

--- board/views.py
+++ board/views.py
@@ -19,7 +19,11 @@ def post_new(request):
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
-			post.author = request.user
+			if request.user == null:
+				post.author = User.objects.create_user('anonymous', 'aa@aa.com', '1234')
+
+			else:
+				post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
 			return redirect('board.views.post_detail', pk=post.pk)
@@ -28,6 +32,18 @@ def post_new(request):
 	return render(request, 'board/post_edit.html', {'form':form})
 
 
-
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
