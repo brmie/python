8679778c7990f23cb851be34e0ba904ad0fd858c
boramkim <brmie@hanmail.net>
2016-10-19 02:33:05 +0900

    edit settings.py

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
