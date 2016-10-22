78cfbfde9a9e8b2537631b7ff5500eb9f44a3c1d
boramkim <brmie@hanmail.net>
2016-10-23 03:20:45 +0900

    add nowPage all

--- board/urls.py
+++ board/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 	url(r'^$', views.main, name='main'),
 	url(r'^list/(?P<nowPage>[0-9]+)/$', views.post_list, name='post_list'),
 	url(r'^post/(?P<nowPage>[0-9]+)/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<nowPage>[0-9]+)/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<nowPage>[0-9]+)/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-	url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
+	url(r'^post/(?P<nowPage>[0-9]+)/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
 ]
--- board/views.py
+++ board/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 from .models import Post
 from .forms import PostForm
 
-
 # def post_list(request):
 # 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 # 	return render(request, 'board/post_list.html', {'posts':showPost, 'pages':showPage})
@@ -73,7 +72,7 @@ def post_detail(request, pk, nowPage):
 	nowPage = int(nowPage)
 	return render(request, 'board/post_detail.html', {'post':post, 'nowPage':nowPage})
 
-def post_new(request):
+def post_new(request, nowPage):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
@@ -86,13 +85,13 @@ def post_new(request):
 				post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
-			return redirect('board.views.post_detail', pk=post.pk)
+			return redirect('board.views.post_detail', pk=post.pk, nowPage=nowPage)
 	else:
 		form = PostForm()
-	return render(request, 'board/post_edit.html', {'form':form})
+	return render(request, 'board/post_edit.html', {'form':form, 'nowPage':nowPage})
 
 
-def post_edit(request, pk):
+def post_edit(request, pk, nowPage):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -105,12 +104,12 @@ def post_edit(request, pk):
             return redirect('board.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'board/post_edit.html', {'form': form})
+    return render(request, 'board/post_edit.html', {'form': form, 'nowPage':nowPage})
 
 
-def post_delete(request, pk):
+def post_delete(request, pk, nowPage):
 	post = get_object_or_404(Post, pk=pk)
 	post.delete()
 
-	return redirect('board.views.post_list')
+	return redirect('board.views.post_list', nowPage=nowPage)
 
