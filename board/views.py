8d4f33efa51b61b332aa90d60a8c4b68306e901f
boramkim <brmie@hanmail.net>
2016-10-22 18:23:26 +0900

    edit views.py int

--- board/views.py
+++ board/views.py
@@ -24,7 +24,7 @@ def post_list(request, nowPage):
 	allPost = posts.count()
 
 	# 현재페이지
-	nowPage = get_object_or_404(integer, nowPage=int(nowPage))
+	nowPage = get_object_or_404(int, nowPage=int(nowPage))
 
 	# 글 몇개씩 자를지
 	cutNum = 4
