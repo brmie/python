ae20bc44f10d1e0a792a5e569e99b2a69ad35f7f
boramkim <brmie@hanmail.net>
2016-10-24 03:34:24 +0900

    script test9

--- board/static/js/default.js
+++ board/static/js/default.js
@@ -5,15 +5,16 @@ $(function(){
 
 	var nowPageNum = location.pathname.split('/')[2];
 	var nowPage = $("a[class='pageNum"+nowPageNum+"']");
-	var notNow = $("a[class=!'pageNum"+nowPageNum+"']");
+	var pages = $("a[class='page']");
 
 	alert(nowPage);
 
+	if(pages.hasClass('nowPage')){
+		pages.removeClass('nowPage')
+	}
+	
 	nowPage.addClass('nowPage');
 
-	if(notNow.hasClass('nowPage')){
-		notNow.removeClass('nowPage')
-	}
 
 	// for(var i=0; i<pages.length; i++){
 	// 	alert(pages[i]);
