bec5e7d55ae1e3adf9936f669c729bd3fee1a816
boramkim <brmie@hanmail.net>
2016-10-24 02:38:44 +0900

    add JQuery

--- board/static/js/default.js
+++ board/static/js/default.js
@@ -2,16 +2,16 @@ window.onload = function(){
 	// location.pathname ==> /list/1/
 
 	var nowPageNum = location.pathname.split('/')[2];
-	var pages = document.getElementsByClassName('page');
+	var $pages = $('.pages');
 
-	for(var i=0; i<pages.length; i++){
-		if(pages[i]==nowPageNum){
+	for(var i=0; i<$pages.length; i++){
+		if($pages[i].hasClass("pageNum"+nowPageNum)){
+			$pages[i].addClass('nowPage');
 			alert('hoho1');
-			pages[i].addClass('nowPage');
 		} else {
-			if(pages[i].hasClass('nowPage')){
+			if($pages[i].hasClass('nowPage')){
+				$pages[i].removeClass('nowPage');
 				alert('hoho2');
-				pages[i].removeClass('nowPage');
 			}
 		}
 	}
