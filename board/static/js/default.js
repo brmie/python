0e6b2e8e360ab527f85f4630747d09eec56c7633
boramkim <brmie@hanmail.net>
2016-10-24 03:26:03 +0900

    script test7

--- board/static/js/default.js
+++ board/static/js/default.js
@@ -4,25 +4,32 @@ $(function(){
 
 
 	var nowPageNum = location.pathname.split('/')[2];
-	var pages = $('.page');
+	var nowPage = $("a[class='pageNum"nowPageNum+"']");
+	var notNow = $("a[class=!'pageNum"nowPageNum+"']");
 
-	alert(pages);
+	alert(nowPage);
 
-	for(var i=0; i<pages.length; i++){
-		alert(pages[i].html());
+	nowPage.addClass('nowPage');
 
-		// if(pages[i].split('list/')[1] == nowPageNum+'/'){
-		// 	alert("hohoho");
-		// }
-
-		// if($pages[i].hasClass("pageNum"+nowPageNum)){
-		// 	$pages[i].addClass('nowPage');
-		// 	alert('hoho1');
-		// } else {
-		// 	if($pages[i].hasClass('nowPage')){
-		// 		$pages[i].removeClass('nowPage');
-		// 		alert('hoho2');
-		// 	}
-		// }
+	if(notNow.hasClass('nowPage')){
+		notNow.removeClass('nowPage')
 	}
+
+	// for(var i=0; i<pages.length; i++){
+	// 	alert(pages[i]);
+
+	// 	// if(pages[i].split('list/')[1] == nowPageNum+'/'){
+	// 	// 	alert("hohoho");
+	// 	// }
+
+	// 	// if($pages[i].hasClass("pageNum"+nowPageNum)){
+	// 	// 	$pages[i].addClass('nowPage');
+	// 	// 	alert('hoho1');
+	// 	// } else {
+	// 	// 	if($pages[i].hasClass('nowPage')){
+	// 	// 		$pages[i].removeClass('nowPage');
+	// 	// 		alert('hoho2');
+	// 	// 	}
+	// 	// }
+	// }
 })
