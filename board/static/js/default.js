da7810b554a144ea2c70f76df5781745cefc4859
boramkim <brmie@hanmail.net>
2016-10-24 02:46:07 +0900

    script test

--- board/static/js/default.js
+++ board/static/js/default.js
@@ -1,11 +1,13 @@
 $(function(){
 	// location.pathname ==> /list/1/
 
-	alert('hohoho');
+
 
 	var nowPageNum = location.pathname.split('/')[2];
 	var $pages = $('.pages');
 
+	alert($pages);
+	
 	for(var i=0; i<$pages.length; i++){
 		if($pages[i].hasClass("pageNum"+nowPageNum)){
 			$pages[i].addClass('nowPage');
