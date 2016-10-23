d0d2dd9eaee8d85933a822b724f7d62a93985d1b
boramkim <brmie@hanmail.net>
2016-10-24 02:00:25 +0900

    add paging color

--- board/static/js/default.js
+++ board/static/js/default.js
@@ -1,6 +1,18 @@
 window.onload = function(){
 	// location.pathname ==> /list/1/
-	var str = location.pathname;
-	alert(str.split('/')[2]);
-	// document.getElementsByClassName(location.pathname)
+
+	var nowPageNum = location.pathname.split('/')[2];
+	var pages = document.getElementsByClassName('page');
+
+	for(var i=0; i<pages.length; i++){
+		if(pages[i]==nowPageNum){
+			alert('hoho1');
+			pages[i].addClass('nowPage');
+		} else {
+			if(pages[i].hasClass('nowPage')){
+				alert('hoho2');
+				pages[i].removeClass('nowPage');
+			}
+		}
+	}
 }
